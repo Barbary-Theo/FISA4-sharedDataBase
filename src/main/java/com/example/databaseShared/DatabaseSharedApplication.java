3b2546231c2ca3aa5bc07d8ca83d68eaace15c99
Barbary-Theo <theo.barbary25@gmail.com>
2022-12-13 10:38:41 +0100

    sus

---
+++ src/main/java/com/example/databaseShared/DatabaseSharedApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseShared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSharedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/databaseShared/DatabaseSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databaseShared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
